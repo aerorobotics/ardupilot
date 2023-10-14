ef5539dd98b8b480aeae9f9ce358b831a0a48d36
matt <anderson_rayner@hotmail.com>
2023-10-14 11:47:13 -0700

    Updates to the docker build scripts

--- build_arcl_ff.sh
+++ build_arcl_ff.sh
@@ -12,26 +12,38 @@ docker run --user $(id -u):$(id -g) -v `pwd`:/ardupilot ardupilot:latest git sub
 # Build CubeOrangePlus version of the code
 echo "Configuring build"
 docker run --user $(id -u):$(id -g) -v `pwd`:/ardupilot ardupilot:latest ./waf configure --board CubeOrangePlus --enable-opendroneid
-docker run --user $(id -u):$(id -g) -v `pwd`:/ardupilot ardupilot:latest ./waf copter
+# docker run --user $(id -u):$(id -g) -v `pwd`:/ardupilot ardupilot:latest ./waf configure --board Pixhawk4 --enable-opendroneid
+# docker run --user $(id -u):$(id -g) -v `pwd`:/ardupilot ardupilot:latest ./waf configure --board CubeOrangePlus-SimOnHardWare --enable-opendroneid
+# docker run --user $(id -u):$(id -g) -v `pwd`:/ardupilot ardupilot:latest ./waf copter
 
 exit 0
 
 # ----
 
 # Start ArduCopter SITL
-docker run -it \
-    --net=host \
-    -v `pwd`:/ardupilot \
-    -u "$(id -u):$(id -g)" \
-    ardupilot:latest sim_vehicle.py -v ArduCopter
+# docker run -it \
+#     --net=host \
+#     -v `pwd`:/ardupilot \
+#     -u "$(id -u):$(id -g)" \
+#     ardupilot:latest sim_vehicle.py -v ArduCopter
 
-exit 0
+# exit 0
 
 # ----
 
 # Start the docker with command line
-docker run -it \
-    --net=host \
-    -v `pwd`:/ardupilot \
-    -u "$(id -u):$(id -g)" \
-    ardupilot:latest bash
+# docker run -it \
+#     --net=host \
+#     -v `pwd`:/ardupilot \
+#     -u "$(id -u):$(id -g)" \
+#     ardupilot:latest bash
+
+# ----
+
+# SITL-on-Hardware
+#docker run -it \
+#    --net=host \
+#    -v `pwd`:/ardupilot \
+#    -u "$(id -u):$(id -g)" \
+#    ardupilot:latest \
+#    ./libraries/SITL/examples/on-hardware/sitl-on-hw.py --board Pixhawk4 --vehicle copter
